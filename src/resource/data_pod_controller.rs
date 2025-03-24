a9744ea3b98401ae71f98c0dacbbb385f83bff5b
Jannis Hochmuth <jannis.hochmuth@computational.bio.uni-giessen.de>
2025-03-24 13:36:55 +0100

    feat: Merged all patches in DataPodController reconcile loop

--- src/resource/data_pod_controller.rs
+++ src/resource/data_pod_controller.rs
@@ -1,6 +1,6 @@
 use crate::resource::crd::{DataNode, DataPod, DataPodSpec, DataPodStatus};
 use k8s_openapi::apimachinery::pkg::apis::meta::v1::{ObjectMeta, OwnerReference};
-use kube::core::PartialObjectMetaExt;
+use kube::api::ListParams;
 use kube::{
     Resource,
     api::{Api, Patch, PatchParams, ResourceExt},
@@ -51,6 +51,7 @@ impl DataPod {
         // Fetch DataPod
         let name = self.name_any();
         let data_pod_api: Api<DataPod> = Api::namespaced(client.clone(), &ns);
+        let data_pod = data_pod_api.get(&name).await?;
 
         // Set path to DataPod uid if None, empty or "/"
         let (new_path, generated) = if self
@@ -70,82 +71,79 @@ impl DataPod {
             (self.spec.path.clone().unwrap(), false) // Cannot fail as the other branch collects all the other cases.
         };
 
-        let path_patch = Patch::Apply(json!({
-            "apiVersion": DataPod::api_version(&()),
-            "kind": DataPod::kind(&()),
-            "spec": DataPodSpec {
-                path: Some(new_path),
-                ..Default::default()
-            }
-        }));
-        data_pod_api
-            .patch(&name, &PatchParams::apply("cntrlr").force(), &path_patch)
-            .await
-            .inspect_err(|e| error!("Failed to patch path: {:?}", e))
-            .map_err(Error::KubeError)?;
-
-        //TODO: Set owner reference and datNodeRef depending on ref/selector
-        if let Some(node_ref) = &self.spec.data_node_ref {
-            let data_node_api: Api<DataNode> = Api::namespaced(client.clone(), &ns);
-            let data_node = data_node_api
+        // Set owner reference and datNodeRef depending on ref/selector
+        let data_node_api: Api<DataNode> = Api::namespaced(client.clone(), &ns);
+        let data_node = if let Some(node_ref) = &self.spec.data_node_ref {
+            data_node_api
                 .get(&node_ref.name)
                 .await
-                .map_err(Error::KubeError)?;
-
-            let owner = OwnerReference {
-                api_version: DataNode::api_version(&()).to_string(),
-                block_owner_deletion: None,
-                controller: None,
-                kind: DataNode::kind(&()).to_string(),
-                name: data_node.name_any(),
-                uid: data_node.metadata.uid.unwrap(),
-            };
-
-            let metadata_patch = Patch::Apply(
-                ObjectMeta {
-                    owner_references: Some(vec![owner]),
-                    ..Default::default()
-                }
-                .into_request_partial::<DataPod>(),
-            );
-            data_pod_api
-                .patch_metadata(&name, &PatchParams::apply("cntrlr"), &metadata_patch)
-                .await?;
-            info!("PartialMetadataPatch for {} succeeded.", &name)
+                .map_err(Error::KubeError)?
         } else if let Some(selector) = &self.spec.data_node_selector {
-            //TODO:
             //  - Fetch DataNodes with selector
             //  - Take first result and set as owner (?)
+            let selector_string = selector
+                .iter()
+                .map(|(k, v)| format!("{}={}", k, v))
+                .collect::<Vec<_>>()
+                .join(",");
+            data_node_api
+                .list(&ListParams::default().labels(&selector_string))
+                .await?
+                .items
+                .first()
+                .expect("No nodes found with provided selector")
+                .clone()
         } else {
-            return Err(Error::ReconcilerError(format!("DataPod {} has no DataNode reference", &name)));
-        }
+            return Err(Error::ReconcilerError(format!(
+                "DataPod {} has no DataNode reference/selector",
+                &name
+            )));
+        };
 
-        // always overwrite status object with what we saw
-        let new_status = Patch::Apply(json!({
+        // Patch owner ref
+        let owner = OwnerReference {
+            api_version: DataNode::api_version(&()).to_string(),
+            block_owner_deletion: None,
+            controller: None,
+            kind: DataNode::kind(&()).to_string(),
+            name: data_node.name_any(),
+            uid: data_node.metadata.uid.unwrap(),
+        };
+
+        // Patch everything at once
+        let data_pod_clone = data_pod.clone();
+        let overall_patch = Patch::Apply(json!({
             "apiVersion": DataPod::api_version(&()),
             "kind": DataPod::kind(&()),
+            "metadata": ObjectMeta {
+                name: Some(data_pod_clone.metadata.name.expect("DataPod is missing name.")),
+                namespace: data_pod_clone.metadata.namespace,
+                owner_references: Some(vec![owner]),
+                ..Default::default()
+            },
+            "spec": DataPodSpec {
+                path: Some(new_path),
+                data_node_ref: data_pod_clone.spec.data_node_ref,
+                data_node_selector: data_pod_clone.spec.data_node_selector,
+                request: data_pod_clone.spec.request,
+            },
             "status": DataPodStatus {
                 available: true,
                 generated_path: generated,
             }
         }));
-
-        //  - Set generated_path if changed
-        trace!("Patching status for {}", name);
-        let ps = PatchParams::apply("cntrlr").force();
         let new_data_pod = data_pod_api
-            .patch_status(&name, &ps, &new_status)
-            .await
-            .inspect_err(|e| error!("Failed to patch status: {:?}", e))
-            .map_err(Error::KubeError)?;
+            .patch(&name, &PatchParams::apply("cntrlr").force(), &overall_patch)
+            .await?;
 
-        if self.status != new_data_pod.status {
+        if data_pod != new_data_pod.to_owned() {
             ctx.recorder
                 .publish(
                     &Event {
                         type_: EventType::Normal,
                         reason: format!(
-                            "Status changed: {:?}",
+                            //"Status changed: {:?}",
+                            "Reconciled: DataPod changed (path generated: {:?})",
                             new_data_pod
                                 .status
                                 .as_ref()
